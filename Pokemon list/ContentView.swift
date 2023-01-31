abbb779623e6627e920c5cc990d199e472a17ea9
Sergi Rojas Serra <sergirojasserra@gmail.com>
2023-01-31 12:04:18 +0100

    Initial Commit

---
+++ Pokemon list/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokemon list
+//
+//  Created by Sergi Rojas on 31/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokemon list/Pokemon_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_listApp.swift
+//  Pokemon list
+//
+//  Created by Sergi Rojas on 31/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
